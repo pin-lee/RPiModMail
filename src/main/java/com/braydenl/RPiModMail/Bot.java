9dcd90ad21c27eb105830a4a593e6f864202f1a8
Brayden L <brayden-l@users.noreply.github.com>
2022-06-23 23:25:59 -0500

    Pretty much all bot fucntionality implemenetd.

--- src/main/java/com/braydenl/RPiModMail/Bot.java
+++ src/main/java/com/braydenl/RPiModMail/Bot.java
@@ -1,14 +1,30 @@
 package com.braydenl.RPiModMail;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Bot extends ListenerAdapter {
+
+    HashMap<User, String> usersAndTickets = new HashMap<>();
+
+    JDA jda;
+    TextChannel modMail;
+    int ticketNumber = 1;
 
     public Bot() {
         System.out.println("lol, lmao");
+    }
+
+    public void configure(JDA jda) {
+        this.jda = jda;
+        this.modMail = jda.getTextChannelById("989330012689813585");
     }
 
     @Override
@@ -18,16 +34,52 @@ public class Bot extends ListenerAdapter {
         if (author.isBot()) return;
         String content = msg.getContentRaw();
 
-
         if (content.equals("ticket") || content.equals("modmail")) {
             if (msg.isFromGuild()) {
                 author.openPrivateChannel().flatMap(channel -> channel.sendMessage("Use `ticket` to create a new ticket!")).queue();
                 return;
             }
-            msg.reply("What up, dawg?").queue();
-            System.out.println(event.getAuthor() + " said Trans rights");
+            msg.reply("What's up? Post text, screenshots, etc concerning this ticket's subject matter. **To finalize the ticket and " +
+                    "send it off, tell me** `ticket complete`**!**").queue();
+            if (!usersAndTickets.containsKey(author)) {
+                usersAndTickets.put(author, "");
+                return;
+            }
         }
 
+        if (!msg.isFromGuild() && content.equals("ticket complete") && usersAndTickets.containsKey(author) && !usersAndTickets.get(author).isEmpty()) {
+            modMail.sendMessage("**NEW MOD MAIL -> [ticket " + ticketNumber + "]**\nUser: <@" + author.getId() + ">\n" + usersAndTickets.get(author)).queue();
+            usersAndTickets.remove(author);
+            ticketNumber++;
+            return;
+        }
+
+        if (!msg.isFromGuild() && content.equals("ticket cancel")) {
+            usersAndTickets.remove(author);
+            msg.reply("Cancelled ticket creation.").queue();
+            return;
+        }
+
+        if (!msg.isFromGuild() && content.equals(("help"))) {
+                author.openPrivateChannel().flatMap(channel -> channel.sendMessage("""
+                        Use `ticket` to create a new ticket.
+                        Use `ticket complete` to close the ticket and send it off to the mod team.
+                        Use `ticket cancel` to cancel the ticket.
+                        *P.S. This help dialogue does not show up in the ticket, so don't worry!*"""
+                        )).queue();
+                return;
+        }
+
+        if (!msg.isFromGuild() && usersAndTickets.containsKey(author)) {
+            List<Message.Attachment> attachments;
+            usersAndTickets.put(author, usersAndTickets.get(author) + "\n" + msg.getContentRaw());
+            try {
+                attachments = msg.getAttachments();
+                for (Message.Attachment attachment : attachments) {
+                    usersAndTickets.put(author, usersAndTickets.get(author) + "\n" + attachment.getUrl());
+                }
+            } catch (Exception ignored) {}
+        }
     }
 
 }
--- src/main/java/com/braydenl/RPiModMail/Launcher.java
+++ src/main/java/com/braydenl/RPiModMail/Launcher.java
@@ -12,20 +12,22 @@ import java.util.Arrays;
 public class Launcher  {
 
     static JDA jda;
+    static Bot bot = new Bot();
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         try {
-            String TOKEN = "";
+            String TOKEN = args[0];
             GatewayIntent[] intents = {
                     GatewayIntent.DIRECT_MESSAGES,
                     GatewayIntent.GUILD_MESSAGES,
             };
             jda = JDABuilder.createLight(TOKEN, Arrays.asList(intents))
-                    .addEventListeners(new Bot())
-                    .setActivity(Activity.watching("#modmail and Direct Messages!"))
+                    .addEventListeners(bot)
+                    .setActivity(Activity.watching("Direct Messages and you!"))
                     .build()
                     .awaitReady();
+            bot.configure(jda);
         } catch (LoginException | InterruptedException e) {
             e.printStackTrace();
             System.out.println("Failed to login.");
